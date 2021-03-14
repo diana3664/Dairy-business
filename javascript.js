5b16f694a4ee33b3c46032f0192e243666e04231
diana3664 <dianamus2017@gmail.com>
2021-03-14 21:32:32 +0300

    declare the month of January

--- javascript.js
+++ javascript.js
@@ -22,6 +22,11 @@ var incomeOvertime = 45;{
     console.log("Your weekly income will be ksh" + " " +incomeOvertime * totalProduction * 7)
     console.log("Your yearly income will be ksh" + " " +incomeOvertime * totalProduction * 366)
 }
+var monthlyProduction = [{
+  month : "January"
+  days : 31
+}
+]
 
 
 
