682341c84789b1b58bf52d7c96f145da466ddda3
diana3664 <dianamus2017@gmail.com>
2021-03-15 19:28:58 +0300

    stlyle the head of our html

--- javascript.js
+++ javascript.js
@@ -17,14 +17,48 @@ var i=0;
 //declare a new variable
 var totalProduction = a+b+c+d;{
   console.log("The total production is" + " " + totalProduction + " "+"liters per day")
+  document.getElementById("totals").innerHTML = "<p>The total production is " +totalProduction + " litres per day</p>"
 };
-var incomeOvertime = 45;{
+var incomeOvertime = 45;
+{
     console.log("Your weekly income will be ksh" + " " +incomeOvertime * totalProduction * 7)
     console.log("Your yearly income will be ksh" + " " +incomeOvertime * totalProduction * 366)
 }
 var month = ["January", "Febuary", "march", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var days = [31, 29 , 31, 30 , 31, 30, 31, 31, 30, 31, 30, 31]
-while(i < month.length ,days.length){
-console.log("Your income for " + month[i] + "is" + days[i] * incomeOvertime * totalProduction )
-i++;
+var days = [31, 29 , 31, 30 , 31, 30, 31, 31, 30, 31, 30, 31];
+
+var i = 0;
+while(i < month.length ){
+  var month = ["January", "Febuary", "march", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+console.log("Your income for " + month[i] + " is " + days[i] * incomeOvertime * totalProduction )
+console.log(days[i]);
+console.log(incomeOvertime);
+console.log(totalProduction);
+i++; 
 };
+
+function triangle() {
+  //create an array to store the sides
+  var shades = [];
+  //push all the sides into the array
+  shades.push(document.getElementById("cow1").value);
+  shades.push(document.getElementById("cow2").value);
+  shades.push(document.getElementById("cow3").value);
+  shades.push(document.getElementById("cow4").value);
+  //assign the values of the array to variables a,b,c
+  var [a,b,c,d] = shades;
+  var display = document.getElementById("display");
+  var explanation = document.getElementById("explanation");
+  if(a.length === 0 || b.length === 0 || c.length === 0 ) {
+    //check whether the user has typed something
+    display.innerHTML = "<p>Please <span class='fill'>FILL</span> all the fields</p>" 
+  } else {
+    //convert the strings to numbers
+    a = parseFloat(a);
+    b = parseFloat(b);
+    c = parseFloat(c);
+    //track the triangle
+    track(a,b,c);
+  }
+}
+
